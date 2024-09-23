c0dc8e76899450d7df75699ac3ab2eb29a5de9f5
francoleon08 <francoleon284@gmail.com>
2024-09-23 19:46:47 -0300

    update

--- src/MiCompSalud.js
+++ src/MiCompSalud.js
@@ -3,9 +3,8 @@ import React from "react";
 function MiCompPolitica() {
     // return a jsx ul list of a menu with five random items
     return <div>
-       <p> Esta es una noticia de salud</p>
-
-        
+       <p>Estrategia focalizada: Salud comienza la distribución de vacunas contra el dengue</p>
+       <a href="https://www.argentina.gob.ar/noticias/estrategia-focalizada-salud-comienza-la-distribucion-de-vacunas-contra-el-dengue">Fuente</a>    
 
     </div>;
 }
